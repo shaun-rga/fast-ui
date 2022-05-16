22d8e959d066e9859a1571960022551017867d3e
shaun-rga <shaun.gavin@rgare.com>
2022-05-16 15:08:50 -0500

    Adding API call to Home component

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from 'react'
+
 const Home = () => {
-  return <h1>Home</h1>
+  const [message, setMessage] = useState({ msg: '' })
+
+  const makeAPICall = async () => {
+    try {
+      const response = await fetch('http://localhost:8080', { mode: 'cors' })
+      const data = await response.json()
+      setMessage(data)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    makeAPICall()
+  }, [])
+
+  return (
+    <>
+      <h1>Home</h1>
+      <p>
+        <b>Message from API:</b> {message.msg !== '' ? message.msg : 'ERROR'}
+      </p>
+    </>
+  )
 }
 
 export default Home
