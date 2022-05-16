9bb1cbc99a5942c0d094adb2e32b2a427896dbcf
shaun-rga <shaun.gavin@rgare.com>
2022-05-16 11:52:16 -0500

    Adding Prettier.

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,11 @@ import CssBaseline from '@mui/material/CssBaseline';
 import App from './Components/App/App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <CssBaseline>
     <App />
-  </CssBaseline>,
+  </CssBaseline>
 );
